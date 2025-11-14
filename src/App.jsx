6aaac39d19d18223725eb024816aa077dd88887f
Kallimar <deepakkallimar10@gmail.com>
2025-11-14 12:40:26 +0530

    Fix Vercel case-sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
-import Login from './pages/login/login'
+import Login from './pages/Login/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { onAuthStateChanged } from 'firebase/auth'
